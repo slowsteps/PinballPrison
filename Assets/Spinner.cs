690db1ba5b84a70163419ef7f25041f496acaf62
peter <hofstedemail@yahoo.com>
2014-04-11 10:25:01 +0200

    Spinner and stone

---
+++ Assets/Spinner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	public float spinningSpeed = 10f;
+			
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward,spinningSpeed*Time.deltaTime);
+	}
+}
