7e1d6c87a7377229163a54ac4aca7003ef5846b5
peter <hofstedemail@yahoo.com>
2014-12-10 19:54:14 +0100

    yesterdays version from home

--- Assets/Resources/Scripts/GUIMessage.cs
+++ Assets/Resources/Scripts/GUIMessage.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUIMessage : MonoBehaviour {
 
 	public static GUIMessage instance;
-	public GameObject textField;
+	public Text textField;
 
 	void Start () {
+		textField = GetComponent<Text>();
 		instance = this;
-		gameObject.SetActive(false);
+		//gameObject.SetActive(false);
 	}
 	
 
 	public void SetText(string inText)
 	{
+		print ("SetText " + inText);
 		gameObject.SetActive(true);
-		textField.guiText.text = inText;
+		textField.text = inText;
 	}
 
 
-	void OnMouseDown()
+	public void ZOnMouseDown()
 	{
 		
 		EventManager.fireEvent(EventManager.EVENT_MESSAGE_OK);
