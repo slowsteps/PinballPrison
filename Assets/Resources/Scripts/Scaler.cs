a58e9f0c02912383b4e7bf5b6401f6761fd55800
peter <hofstedemail@yahoo.com>
2014-12-22 00:38:52 +0100

    extra options for Scaler script

--- Assets/Resources/Scripts/Scaler.cs
+++ Assets/Resources/Scripts/Scaler.cs
@@ -7,17 +7,30 @@ public class Scaler : MonoBehaviour {
 	public float TimeOffset = 0f; // wait before kicking off the animation
 	public float ScaleTime = 1f; // time in seconds from original scale to zero scale
 	public float LoopDelay = 0f; // pause before loop rewind
-	public Vector3 toScale;
+	//public Vector3 toScale;
+	public float MaxScale = 1f;
+	public float MinScale = 0f;
+	public bool IsInverted = false;
+	private Vector3 OrigScale;
 
 	void Start()
 	{
+		OrigScale = transform.localScale;
+		transform.localScale = OrigScale * MaxScale;
 		Invoke("StartScaleAnimation",TimeOffset);
 	}
 	
 	private void StartScaleAnimation()
 	{
 		//check http://itween.pixelplacement.com/documentation.php for help on iTween
-		iTween.ScaleTo(gameObject,iTween.Hash("scale",toScale,"time",ScaleTime,"delay",LoopDelay,"looptype","loop","easetype",iTween.EaseType.linear));	
+		if (IsInverted)
+		{
+			iTween.ScaleFrom(gameObject,iTween.Hash("scale",OrigScale*MinScale,"time",ScaleTime,"delay",LoopDelay,"looptype","loop","easetype",iTween.EaseType.linear));			
+		}
+		else
+		{
+			iTween.ScaleTo(gameObject,iTween.Hash("scale",OrigScale*MinScale,"time",ScaleTime,"delay",LoopDelay,"looptype","loop","easetype",iTween.EaseType.linear));	
+		}
 	}
 	
 }
