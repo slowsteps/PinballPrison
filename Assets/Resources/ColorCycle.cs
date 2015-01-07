d3f03ba0151a1f53017984cf3efd976c646f3f3e
peter <hofstedemail@yahoo.com>
2015-01-08 00:01:17 +0100

    ColorCycle fixes

--- Assets/Resources/ColorCycle.cs
+++ Assets/Resources/ColorCycle.cs
@@ -8,9 +8,9 @@ public class ColorCycle : MonoBehaviour {
 	public Color32 ToColor;
 	public float Duration=1f;
 	public float Delay=0f;
-	public Patterns Pattern = Patterns.Sin;
+	public Patterns Pattern = Patterns.Triangle;
 	private SpriteRenderer sprite;
-	private float ratio;
+	public float ratio;
 	private	float MyTime;
 	 
 	public enum Patterns {Sin,Triangle,Saw,Block};
@@ -26,8 +26,8 @@ public class ColorCycle : MonoBehaviour {
 	{
 		MyTime = Time.time + Delay;
 		if (Pattern == Patterns.Sin) ratio = 0.5f + (0.5f*Mathf.Sin(MyTime/Duration));
-		if (Pattern == Patterns.Saw) ratio = Mathf.Repeat(MyTime,Duration);
-		if (Pattern == Patterns.Triangle) ratio = Mathf.PingPong(MyTime,Duration);
+		if (Pattern == Patterns.Saw) ratio = Mathf.Repeat(MyTime/Duration,1f);
+		if (Pattern == Patterns.Triangle) ratio = Mathf.PingPong(MyTime/Duration,1f);
 		if (Pattern == Patterns.Block) ratio =  Mathf.Round(Mathf.Repeat(MyTime/Duration,1f));
 		
 		sprite.color = Color32.Lerp(FromColor,ToColor,ratio);
