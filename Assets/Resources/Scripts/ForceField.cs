44cc48824cc8ad83c60e6342be06ec7bef82a30b
peter <hofstedemail@yahoo.com>
2015-01-08 22:37:47 +0100

    Local forces in Bumper option

--- Assets/Resources/Scripts/ForceField.cs
+++ Assets/Resources/Scripts/ForceField.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class ForceField : MonoBehaviour {
 
 	public Vector2 bumpForce;
+	public bool isLocal = false;
 
 	public void OnTriggerStay2D (Collider2D ball)
 	{
 		if (ball.tag == "ball") 
 		{
-			ball.rigidbody2D.AddForce(bumpForce);
+			if(isLocal) ball.rigidbody2D.AddForce(transform.rotation * bumpForce);
+			else ball.rigidbody2D.AddForce(bumpForce);
 		}
 	}
 }
