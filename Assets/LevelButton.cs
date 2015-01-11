a36db7735c9e80c9b237d7fe73f75ecd1b945df1
peter <hofstedemail@yahoo.com>
2015-01-11 23:45:17 +0100

    Level icons

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+	public LevelThemes CurLevelTheme;
+	public levelStates CurLevelState;
+	public Image ThemeImage;
+	public Text LevelNumberLabel;
+	public int LevelNumber=1;
+	//public 
+	
+	public enum LevelThemes {Square,Diamond,Hexagon,Circle};
+	public enum levelStates {Locked,Playable,MinimalOneChallengeDone,AllChallengesDone};
+
+	// Use this for initialization
+	void Start () 
+	{
+		//LevelNumberLabel.text = LevelNumber;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
