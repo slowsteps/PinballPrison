cd1b6f1a26b44a0a4db2761cea501b41ca05229b
peter <hofstedemail@yahoo.com>
2016-01-17 12:13:33 +0100

    Sunday morning

---
+++ Assets/CameraPostEffects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPostEffects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+//	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+//		Graphics.Blit(src, dest, mat);
+//	}
+}
---
+++ Assets/GUIBallEndOfLevel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIBallEndOfLevel : MonoBehaviour {
+
+	private Text ballLabel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ballLabel = gameObject.GetComponent<Text>();
+		EventManager.Subscribe(OnEvent);
+	}
+	
+	public void OnEvent(string customEvent)
+	{
+		switch(customEvent)
+		{
+		case EventManager.EVENT_BALLS_UPDATED:
+			ballLabel.text = "You have " + GameManager.instance.balls + " balls ";
+			break;
+		}
+	}
+	
+	
+}
