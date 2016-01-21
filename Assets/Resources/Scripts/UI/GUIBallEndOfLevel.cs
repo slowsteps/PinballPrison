20a1c79c45a4176f2009981ff1b222fb8f6d1dd3
peter <hofstedemail@yahoo.com>
2016-01-21 21:32:53 +0100

    first try extra ball countdown

--- Assets/Resources/Scripts/UI/GUIBallEndOfLevel.cs
+++ Assets/Resources/Scripts/UI/GUIBallEndOfLevel.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class GUIBallEndOfLevel : MonoBehaviour {
 
 	private Text ballLabel;
+	private int extraBallWaitDuration = 30;
 
 	// Use this for initialization
 	void Start () 
@@ -19,8 +20,28 @@ public class GUIBallEndOfLevel : MonoBehaviour {
 		{
 		case EventManager.EVENT_BALLS_UPDATED:
 			ballLabel.text = "You have " + GameManager.instance.balls + " balls ";
+			if (GameManager.instance.balls == 0) 
+			{
+				StartCoroutine(CountdownToExtraBall());
+			}
+			else StopCoroutine(CountdownToExtraBall());
+			
 			break;
 		}
+	}
+	
+	
+	public IEnumerator CountdownToExtraBall()
+	{
+		while (extraBallWaitDuration > 0 )
+		{
+			ballLabel.text = extraBallWaitDuration + " seconds to extra ball ";
+			extraBallWaitDuration--;
+			if (extraBallWaitDuration == 0) GameManager.instance.AddBall();
+			yield return new WaitForSeconds(1);
+		}
+		
+	
 	}
 	
 	
